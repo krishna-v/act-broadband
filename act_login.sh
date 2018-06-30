1490c876d0134a314b3d695da7abd6b1cbb15dd3
Krishna V <krishna.visweswaran@gmail.com>
2018-06-30 11:27:39 +0530

    Add files via upload
    Initial Commit

---
+++ act_login.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#PORTALPAGE="http://portal.actcorp.in/web/blr/home"
+PORTALPAGE="https://selfcare.actcorp.in/web/blr/home"
+PORTALFILE="/tmp/actportal.html"
+OUTFILE="/tmp/act_login.html"
+UID_NAME="_login_WAR_BeamPromotionalNDownloadsportlet_uname"
+PWD_NAME="pword"
+IP_NAME="userIP"
+ACT_CONF_FILE="/etc/actbroadband/act.conf"
+
+if [ -f ${ACT_CONF_FILE} ]; then
+	source ${ACT_CONF_FILE}
+fi
+
+vprint() {
+	if [ ${VERBOSE:-0} == 1 ]; then
+		echo $*
+	fi
+}
+
+processargs() {
+	while [[ $# -gt 0 ]]
+	do
+		case $1 in
+		-v|--verbose)
+			VERBOSE=1
+		;;
+		-i|--interface)
+			ACT_IF=$2
+			shift
+		;;
+		-u|--user)
+			USERID=$2
+			shift
+		;;
+		-p|--password)
+			PASSWORD=$2
+			shift
+		;;
+		esac
+		shift
+	done
+}
+
+processargs $*
+
+IPADDR=$(ifconfig ${ACT_IF} | grep 'inet addr' | cut -d':' -f 2 | cut -d' ' -f 1)
+
+curl --silent -o ${PORTALFILE} ${PORTALPAGE}
+URL=$(egrep '?p_auth=' ${PORTALFILE} | egrep 'log(in|out)' | cut -d'"' -f2)
+vprint "URL is $URL"
+
+if [ -z "$URL" ]; then
+	vprint "Could not find login URL in portal page"
+	exit 1
+fi
+
+echo ${URL} | grep -q login
+LOGGED_IN=$?
+
+if [ ${LOGGED_IN} == 1 ]; then
+	vprint "Already logged in"
+	exit 0
+fi
+
+curl --silent --data "${IP_NAME}=${IPADDR}&${UID_NAME}=${USERID}&${PWD_NAME}=${PASSWORD}" -o ${OUTFILE} ${URL}
