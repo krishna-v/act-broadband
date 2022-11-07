91e6d87aa66c06721c6b6c5d1f804972face9e79
krishna-v <krishna.visweswaran@gmail.com>
2022-11-07 21:54:01 +0530

    Cleaned up lots of pylint complaints, and added some docstrings.

--- act-connect.py
+++ act-connect.py
@@ -1,17 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-import requests
+'''
+act-connet.py
+Author: Krishna V
+https://github/com/krishna-v/act-broadband/act-connect.py
+
+Description: Script to interact with the new version portal of ACT Broadband
+(circa Nov' 2022) 
+
+act-connect.py -h for usage.
+'''
+
+import os
+import time
+import re
+import base64
 import json
+from configparser import ConfigParser
+import argparse
+import netifaces as ni
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad, unpad
-import base64
-import netifaces as ni
-from configparser import ConfigParser
 import jwt
-import re
-import time
-import os
-import argparse
+import requests
 
 headers = {
     'Accept': 'application/json, text/plain, */*',
@@ -29,57 +40,63 @@ headers = {
     'ngsw-bypass': 'true',
     'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
     'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"Accept: application/json, text/plain, */*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Connection': 'keep-alive',
     'sec-ch-ua-platform': '"Windows"'
 }
 
-act_baseurl = "https://selfcare.actcorp.in"
-act_tokenfile = "/tmp/act_token"
-conf_file = "/etc/actbroadband/act.conf"
+ACT_BASEURL = "https://selfcare.actcorp.in"
+ACT_TOKENFILE = "/tmp/act_token"
+CONF_FILE = "/etc/actbroadband/act.conf"
 
 cookies = {}
 globalvars = {}
 conf = {}
 
 def dprint(lvl, *args, **kwargs):
+    ''' print(or not) based on verbosity level '''
     if lvl <= globalvars['verbosity']:
         print(*args, **kwargs)
 
-def loadConf():
+def load_conf():
+    '''
+    Load configuration file /etc/actbroadband/act.conf, containing username and password.
+    Supports the old sectionless conf format used by act_login.sh, so this method is pretty hacky.
+    '''
     parser = ConfigParser()
-    with open(conf_file, 'rb') as cfg:
+    with open(CONF_FILE, 'rb') as cfg:
         parser.read_string("[cfg]\n" + cfg.read().decode('utf-8'))
     global conf
     conf = parser._sections['cfg']
 
-def getConf(key):
+def get_conf(key):
+    ''' get configuration item '''
     return conf[key.lower()].strip('"')
-    
 
-def getCipher():
+def get_cipher():
+    ''' create a new instance of AES cipher '''
     key = globalvars['key'].encode('utf-8')
     iv = globalvars['iv'].encode('utf-8')
     return AES.new(key, AES.MODE_CBC, iv=iv)
 
 def encrypt(data):
-    cipher = getCipher()
+    ''' encrypt a piece of data'''
+    cipher = get_cipher()
     cyphertext = cipher.encrypt(pad(data, AES.block_size))
     return base64.b64encode(cyphertext).decode('utf-8')
 
 def decrypt(data):
-    cipher = getCipher()
+    ''' decrypt a piece of data'''
+    cipher = get_cipher()
     cyphertext = base64.b64decode(data)
     plaintext = unpad(cipher.decrypt(cyphertext), AES.block_size)
     return plaintext.decode('utf-8')
 
-def do_post(url, data, cookies, headers):
-    return requests.post(url, data=data, cookies=cookies, headers=headers)
+def do_post(url, data, cookies_, headers_):
+    ''' do a post request '''
+    return requests.post(url, data=data, cookies=cookies_, headers=headers_)
 
-def scrapeHomePage():
-    url = f"{act_baseurl}/home"
+def scrape_homepage():
+    ''' extract cookie and the name of the .js file containing the crypto keys from the ACT portal. '''
+    url = f"{ACT_BASEURL}/home"
     response = requests.get(url, headers=headers)
     dprint(1, f"{url}: Status {response.status_code}")
     if response.status_code == 200:
@@ -87,13 +104,15 @@ def scrapeHomePage():
         for cookie in response.cookies:
             cookies[cookie.name]= cookie.value
             dprint(2, f"Cookie: {cookie.name} = {cookie.value}")
-        m = re.search('<script src="(main-es2015.+?\.js)"', retdata)
+        m = re.search(r'<script src="(main-es2015.+?\.js)"', retdata)
         if m:
             dprint(3, f"Found script {m.group(1)}")
             return m.group(1)
+    return None
 
-def scrapeScript(script):
-    url = f"{act_baseurl}/{script}"
+def scrape_script(script):
+    ''' extract crypto keys from the javascript file '''
+    url = f"{ACT_BASEURL}/{script}"
     response = requests.get(url, headers=headers)
     dprint(1, f"{url}: Status {response.status_code}")
     if response.status_code == 200:
@@ -106,16 +125,17 @@ def scrapeScript(script):
         if m:
             dprint(3, "SecretKey:", m.group(1))
             globalvars['key'] = m.group(1)
-        m = re.search('authKey:this.service.encrypt\("(.+?)"\)', retdata)
+        m = re.search(r'authKey:this.service.encrypt\("(.+?)"\)', retdata)
         if m:
             dprint(3, "AuthKey:", m.group(1))
             globalvars['authkey'] = m.group(1)
 
-def loadToken():
-    if not os.path.isfile(act_tokenfile):
+def load_token():
+    ''' load previously saved JWT token from local file '''
+    if not os.path.isfile(ACT_TOKENFILE):
         dprint(1, "No tokenfile found")
         return
-    with open(act_tokenfile, 'rb') as tk:
+    with open(ACT_TOKENFILE, 'rb') as tk:
         jwtToken = tk.read()
         output = jwt.decode(jwtToken, globalvars['authkey'],
                     algorithms=["HS256", "HS384", "HS512"],
@@ -128,16 +148,17 @@ def loadToken():
             globalvars['jwt_exp'] = output['exp']
         else:
             dprint(2, "Token expired. Ignoring.")
-            
 
 def initialize():
-    script = scrapeHomePage()
+    ''' do initialization functions '''
+    script = scrape_homepage()
     if script:
-        scrapeScript(script)
-    loadToken()
+        scrape_script(script)
+    load_token()
 
-def checkValidUser():
-    url = f"{act_baseurl}/v1/subscriberdetails/profile/checkValidUser"
+def check_valid_user():
+    ''' check if user is valid. the ACT portal does this before login, but it doesn't appear necessary '''
+    url = f"{ACT_BASEURL}/v1/subscriberdetails/profile/checkValidUser"
     data = { "userId": f"{globalvars['b64user']}" }
     response = do_post(url, json.dumps(data), cookies, headers)
     dprint(1, f"{url}: Status {response.status_code}")
@@ -150,8 +171,9 @@ def checkValidUser():
         dprint(2, f"Data: {retdata}")
     return response.status_code
 
-def loginByUserId():
-    url = f"{act_baseurl}/v1/subscriberlogin/loginByUserId"
+def login_by_userid():
+    ''' login using the user-id and password provided by ACT. '''
+    url = f"{ACT_BASEURL}/v1/subscriberlogin/loginByUserId"
     data = {
         "userId": f"{globalvars['b64user']}",
         "userPwd": f"{globalvars['b64pass']}",
@@ -168,12 +190,13 @@ def loginByUserId():
         retdata['errorCode'] = decrypt(retdata['errorCode'])
         dprint(2, f"Data: {retdata}")
     return response.status_code
-    
+
 def authenticate():
-    url = f"{act_baseurl}/v1/jwt/authenticate"
+    ''' get a JWT token. This is required by some of the functions on the portal. '''
+    url = f"{ACT_BASEURL}/v1/jwt/authenticate"
     plain_authkey = globalvars['authkey']
     b64authkey = encrypt(plain_authkey.encode('utf-8'))
-    plain_source = f"selfcare_{getConf('USERID')}"
+    plain_source = f"selfcare_{get_conf('USERID')}"
     b64source = encrypt(plain_source.encode('utf-8'))
     data = {
         "source": b64source,
@@ -191,20 +214,28 @@ def authenticate():
         dprint(2, output)
         globalvars['jwtToken'] = jwtToken
         globalvars['jwt_exp'] = output['exp']
-        with open(act_tokenfile, 'wb') as tk:
+        with open(ACT_TOKENFILE, 'wb') as tk:
             tk.write(jwtToken.encode('utf-8'))
     return response.status_code
 
-def getToken():
+def get_token():
+    '''
+    check if we have a valid token and return it,
+    else call authenticate to get a fresh one.
+    '''
     now = int(time.time())
     if globalvars.get('jwt_exp', 0) < now + 30:
         authenticate()
     return globalvars['jwtToken']
 
-def userInfo(silent):
-    url = f"{act_baseurl}/profile/userInfo"
+def user_info(silent):
+    '''
+    get user information. This also gets the user city, which is needed in other functions,
+    so we call it silently if we need to use it for that purpose
+    '''
+    url = f"{ACT_BASEURL}/profile/userInfo"
     req_headers = headers.copy()
-    req_headers['Authorization'] = f"Bearer {getToken()}"
+    req_headers['Authorization'] = f"Bearer {get_token()}"
     data = { "accountNo": f"{globalvars['b64user']}" }
     response = do_post(url, json.dumps(data), cookies, req_headers)
     dprint(1, f"{url}: Status {response.status_code}")
@@ -219,11 +250,12 @@ def userInfo(silent):
         retdata['cellulerPhoneNo'] = decrypt(retdata['cellulerPhoneNo'])
         dprint(silent, retdata)
     return response.status_code
-    
-def encodedAuthToken():
-    url = f"{act_baseurl}/v1/servicerequest/encodedAuthToken"
+
+def encoded_auth_token():
+    ''' the ACT portal does this but we don't seem to use it. Included for completeness '''
+    url = f"{ACT_BASEURL}/v1/servicerequest/encodedAuthToken"
     req_headers = headers.copy()
-    req_headers['Authorization'] = f"Bearer {getToken()}"
+    req_headers['Authorization'] = f"Bearer {get_token()}"
     data = {
         "accountNo": f"{globalvars['b64user']}",
         "userId": f"{globalvars['b64user']}"
@@ -237,11 +269,13 @@ def encodedAuthToken():
         print(retdata)
     return response.status_code
 
-def locationDetails(city):
-    url = f"{act_baseurl}/v1/ippool/locationDetails"
-    if not 'city' in globalvars:
-        userInfo(1)
-    data = { "location": f"{globalvars['city']}" }
+def location_details(city):
+    '''
+    get information about ACT infra for a city.
+    This is required to get usage and plan info
+    '''
+    url = f"{ACT_BASEURL}/v1/ippool/locationDetails"
+    data = { "location": city }
     response = do_post(url, json.dumps(data), cookies, headers)
     dprint(1, f"{url}: Status {response.status_code}")
     if response.status_code == 200:
@@ -250,19 +284,20 @@ def locationDetails(city):
         return retdata
     return None
 
-def usageDetails():
-    if not 'city' in globalvars:
-        userInfo(1)
-    locInfo = locationDetails(globalvars['city'])
-    url = f"{act_baseurl}/v1/user/usage/usageDetails/"
+def usage_details():
+    ''' get usage information for this account '''
+    if 'city' not in globalvars:
+        user_info(1)
+    locinfo = location_details(globalvars['city'])
+    url = f"{ACT_BASEURL}/v1/user/usage/usageDetails/"
     req_headers = headers.copy()
-    req_headers['Authorization'] = f"Bearer {getToken()}"
+    req_headers['Authorization'] = f"Bearer {get_token()}"
     data = {
         "accountNo": f"{globalvars['b64user']}",
         "userId": f"{globalvars['b64user']}",
         "phone": f"{globalvars['b64phone']}",
-        "location": locInfo['locationCode'],
-        "ecType": locInfo['ecType']
+        "location": locinfo['locationCode'],
+        "ecType": locinfo['ecType']
     }
     response = do_post(url, json.dumps(data), cookies, req_headers)
     dprint(1, f"{url}: Status {response.status_code}")
@@ -271,10 +306,11 @@ def usageDetails():
         print(retdata)
     return response.status_code
 
-def agreementInfo():
-    url = f"{act_baseurl}/v1/user/usage/agreementInfo"
+def agreement_info():
+    ''' get the plan details for this account '''
+    url = f"{ACT_BASEURL}/v1/user/usage/agreementInfo"
     req_headers = headers.copy()
-    req_headers['Authorization'] = f"Bearer {getToken()}"
+    req_headers['Authorization'] = f"Bearer {get_token()}"
     data = {
         "accountNo": f"{globalvars['b64user']}",
         "userId": f"{globalvars['b64user']}",
@@ -290,41 +326,45 @@ def agreementInfo():
     return response.status_code
 
 def main():
+    ''' main. Parse arguments and execute the appropriate functions '''
     parser = argparse.ArgumentParser(description='Interact with ACT Broadband')
-    parser.add_argument('-v', '--verbose', action='count', default=0, help='Be verbose. Repeat for even more verbosity (e.g. -vvv')
-    parser.add_argument('-c', '--check', action='store_true', help='check if user is valid')
-    parser.add_argument('-l', '--login', action='store_true', help='Log in to ACT Broadband account')
-    parser.add_argument('-i', '--info', action='store_true', help='Retrieve Account User Information')
-    parser.add_argument('-u', '--usage', action='store_true', help='Retrieve Usage Details')
-    parser.add_argument('-p', '--plan', action='store_true', help='Retrieve Plan Details')
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                            help='Be verbose. Repeat for even more verbosity (e.g. -vvv')
+    parser.add_argument('-c', '--check', action='store_true',
+                            help='check if user is valid')
+    parser.add_argument('-l', '--login', action='store_true',
+                            help='Log in to ACT Broadband account')
+    parser.add_argument('-i', '--info', action='store_true',
+                            help='Retrieve Account User Information')
+    parser.add_argument('-u', '--usage', action='store_true',
+                            help='Retrieve Usage Details')
+    parser.add_argument('-p', '--plan', action='store_true',
+                            help='Retrieve Plan Details')
     args = parser.parse_args()
     globalvars['verbosity'] = args.verbose
 
-    loadConf()
+    load_conf()
     initialize()
 
-    globalvars['b64user'] = encrypt(getConf('USERID').encode('utf-8'))
-    globalvars['b64pass'] = encrypt(getConf('PASSWORD').encode('utf-8'))
-    wan_ip = ni.ifaddresses(getConf('ACT_IF'))[ni.AF_INET][0]['addr']
+    globalvars['b64user'] = encrypt(get_conf('USERID').encode('utf-8'))
+    globalvars['b64pass'] = encrypt(get_conf('PASSWORD').encode('utf-8'))
+    wan_ip = ni.ifaddresses(get_conf('ACT_IF'))[ni.AF_INET][0]['addr']
     globalvars['ipaddr'] = wan_ip
 
     if args.check:
-        checkValidUser()
+        check_valid_user()
 
     if args.login:
-        loginByUserId()
+        login_by_userid()
 
     if args.info:
-        userInfo(0)
+        user_info(0)
 
     if args.usage:
-        usageDetails()
+        usage_details()
 
     if args.plan:
-        agreementInfo()
-    
+        agreement_info()
 
 if __name__ == "__main__":
     main()
-
-
